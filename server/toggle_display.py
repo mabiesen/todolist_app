1dcdffea79979a75d6fe5299eb74ba6f1c47d19e
mabiesen <mabiesen@outlook.com>
2017-04-17 12:06:47 -0400

    modified toggle display

--- server/toggle_display.py
+++ server/toggle_display.py
@@ -5,6 +5,21 @@ import subprocess
 import time
 import RPI.GPIO as GPIO
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
+while True:
+       i=GPIO.input(11)
+       if i==0:                 #When output from motion sensor is LOW
+             print "No Movement",i
+			 subprocess.call('xset dpms force off', shell=True)
+             time.sleep(0.1)
+       elif i==1:               #When output from motion sensor is HIGH
+             print "Visitor detected",i
+			 subprocess.call('xset dpms force on', shell=True)
+             time.sleep(180)
+
+
 # os.environ['DISPLAY'] = ":0"
 # GPIO.setmode(GPIO.BCM)
 # pir_pin = 13
@@ -25,18 +40,3 @@ import RPI.GPIO as GPIO
 #                 subprocess.call('xset dpms force off', shell=True)
 #                 displayison = False
 #     time.sleep(1)
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
-GPIO.setup(3, GPIO.OUT)         #LED output pin
-while True:
-       i=GPIO.input(11)
-       if i==0:                 #When output from motion sensor is LOW
-             print "No intruders",i
-             GPIO.output(3, 0)  #Turn OFF LED
-             time.sleep(0.1)
-       elif i==1:               #When output from motion sensor is HIGH
-             print "Intruder detected",i
-             GPIO.output(3, 1)  #Turn ON LED
-             time.sleep(3)
