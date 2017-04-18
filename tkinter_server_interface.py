d08c3c4ea604e580db5921697c2844e40277b152
mabiesen <mabiesen@outlook.com>
2017-04-18 16:22:44 -0400

    blah

--- tkinter_server_interface.py
+++ tkinter_server_interface.py
@@ -10,19 +10,26 @@ import platform
 myplatform = platform.system()
 
 class App(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master)
-        self.grid()
-        self.a()
+	def __init__(self, master):
+		Frame.__init__(self, master)
+		self.grid()
+		self.a()
 
-    def a(self):
+	def a(self):
 		x = 0
 		while x < 5:
-			self.call_button = Button(self, text = "Call")
-			self.call_button['command'] = lambda b=self.call_button: b.destroy()
+			self.call_button = Button(self, text = "Call" + str(x))
+			self.call_button['command'] = lambda b=self.call_button: self.myfunction(b)
 			self.call_button.grid(row=x, column=5) # This is fixing your issue
 			x = x + 1
+
+	def myfunction(self,b):
+		b.destroy()
 
 root = Tk()
 app = App(master=root)
 app.mainloop()
+
+
+#command to destroy a button on click
+		#	self.call_button['command'] = lambda b=self.call_button: b.destroy()
