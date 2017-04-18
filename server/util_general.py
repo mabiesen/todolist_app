ef642e9d387707d56467fbcefa4632bbb5d4cba8
mabiesen <mabiesen@outlook.com>
2017-04-18 00:25:54 -0400

    completed python one to twod array

--- server/util_general.py
+++ server/util_general.py
@@ -18,15 +18,20 @@ def one_to_twod_array(initialarray, numcols, numrows):
 		masterarray.append([])
 		actr = actr + 1
 
+	# Iterate number of rows and columns provided in args
 	while rowctr < numrows:
-
 		while colctr < numcols:
-			item = initialarray(masterctr)
-			# Now on each iteration, we append accordingly
+			item = initialarray[masterctr]
+			# Now on each iteration, we append to each row
 			masterarray[rowctr].append(item)
 			colctr = colctr + 1
 			masterctr = masterctr + 1
 		colctr = 0
 		rowctr = rowctr + 1
-
 	return masterarray
+
+##array test data
+##myarray = [1,2,3,4,5,6,7,8,9]
+##newarray = one_to_twod_array(myarray, 3,3)
+##
+##print(newarray[1][0])
